9ecb9a006554777993f67930cb2471c52775b0af
Mohit Taneja <113243835+mohittaneja156@users.noreply.github.com>
2024-09-25 16:16:32 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -28,7 +28,7 @@ if(isset($_POST['submitContact'])){
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'mtaneja306@gmail.com';                     //SMTP username
-        $mail->Password   = 'dduagsnxircwtmkw';                               //SMTP password
+        $mail->Password   = 'Password';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
